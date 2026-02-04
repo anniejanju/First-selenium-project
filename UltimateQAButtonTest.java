72d0077a90209d12da1932c40fb65b6bab4f0595
anniejanju <99857836+anniejanju@users.noreply.github.com>
2026-02-04 21:25:11 +0530

    Add files via upload

--- UltimateQAButtonTest.java
+++ UltimateQAButtonTest.java
@@ -1,7 +1,11 @@
 package seleniumproject;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UltimateQAButtonTest {
 
@@ -14,9 +18,17 @@ public class UltimateQAButtonTest {
 		// driver.findElement(By.linkText("Big page with many elements")).click();
 		// driver.findElement(By.linkText("Fake Landing Page")).click();
 		 driver.findElement(By.linkText("Fill out forms")).click();
-		 driver.findElement(By.id("name")).sendKeys("Annie");
-		 driver.findElement(By.id("message")).sendKeys("Hello");
-		 driver.findElement(By.linkText("Submit")).click();
+		  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("et_pb_contact_name_0")))
+          .sendKeys("Annie");
+
+      // wait for the Message field then type
+      wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("et_pb_contact_message_0")))
+          .sendKeys("Hello");
+
+      // wait for submit button then click
+      wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".et_pb_contact_submit ")))
+          .click();
 		 
 //driver.findElement(By.linkText("View Courses")).click();
 	}
